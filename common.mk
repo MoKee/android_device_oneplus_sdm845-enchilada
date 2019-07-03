029b42e76af6661787d4dcd6d8da9a3f02e31ba1
Chenxi Mao <chenxi.mao2013@gmail.com>
2019-07-03 19:18:38 +0800

    OP6: Wechat and Alipay support
    Change-Id: Ib392cff6d1cac877dfc9407d1aee474816b74ad5

--- common.mk
+++ common.mk
@@ -162,3 +162,10 @@ PRODUCT_BOOT_JARS += \
 # Config panel
 PRODUCT_PACKAGES += \
     ConfigPanel
+
+# IFAA manager
+PRODUCT_PACKAGES += \
+    org.ifaa.android.manager
+
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
